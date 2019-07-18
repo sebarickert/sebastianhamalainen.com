340c5c902e304f53c874895304471b1368d809de
Sebastian <sebastian@hamse.fi>
2019-07-18 20:44:46 +0300

    Install packages for content management

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,28 @@
 module.exports = {
-	plugins: [ 'gatsby-plugin-sass' ]
+	plugins: [
+		'gatsby-plugin-sass',
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: 'src',
+				path: `${__dirname}/src/`
+			}
+		},
+		'gatsby-plugin-sharp',
+		{
+			resolve: 'gatsby-transformer-remark',
+			options: {
+				plugins: [
+					'gatsby-remark-relative-images',
+					{
+						resolve: 'gatsby-remark-images',
+						options: {
+							maxWidth: 750,
+							linkImagesToOriginal: false
+						}
+					}
+				]
+			}
+		}
+	]
 };
