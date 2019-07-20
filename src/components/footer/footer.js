bb870ab5ba64d0357ccd1ab104bc06ac87a63346
Sebastian <sebastian@hamse.fi>
2019-07-20 14:50:07 +0300

    Update footer styling

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,32 +1,40 @@
 import React from 'react';
 
+import './footer.scss';
+import './menu-footer.scss';
+
 const Footer = () => (
 	<footer id="contact" className="footer">
 		<div className="footer__content container container--inset">
-			<nav aria-label="Footer Navigation">
-				<ul className="menu-footer">
-					<li className="menu-footer__item">
-						<a href="mailto:contact@hamse.fi" className="menu-footer__link h1">
-							E-mail
-						</a>
-					</li>
-					<li className="menu-footer__item">
-						<a href="https://twitter.com/iamsebh" className="menu-footer__link h1">
-							Twitter
-						</a>
-					</li>
-					<li className="menu-footer__item">
-						<a href="https://github.com/shamalainen" className="menu-footer__link h1">
-							Github
-						</a>
-					</li>
-					<li className="menu-footer__item">
-						<a href="https://linkedin.com/in/sebastianhamalainen/" className="menu-footer__link h1">
-							LinkedIn
-						</a>
-					</li>
-				</ul>
-			</nav>
+			<div className="footer__item">
+				<h2 className="footer__heading">Get in touch</h2>
+				<a href="mailto:contact@hamse.fi" className="footer__link">
+					contact@hamse.fi
+				</a>
+			</div>
+			<div className="footer__item">
+				<h2 className="footer__heading">On the web</h2>
+				<nav aria-label="Footer Navigation">
+					<ul className="menu-footer">
+						<li className="menu-footer__item">
+							<a href="https://twitter.com/iamsebh" className="footer__link">
+								Twitter
+							</a>
+						</li>
+						<li className="menu-footer__item">
+							<a href="https://github.com/shamalainen" className="footer__link">
+								Github
+							</a>
+						</li>
+						<li className="menu-footer__item">
+							<a href="https://linkedin.com/in/sebastianhamalainen/" className="footer__link">
+								LinkedIn
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<span className="footer__copyright">© 2019 Sebastian</span>
 		</div>
 	</footer>
 );
