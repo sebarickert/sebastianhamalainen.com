6118b003e8d8b5536766e0a5379280924930cfe1
Sebastian <sebastian@hamse.fi>
2019-07-24 09:31:31 +0300

    Update NotFoundPage to use Hero component

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,15 +3,15 @@ import { Link } from 'gatsby';
 
 import Layout from '../components/layout/layout';
 import Container from '../components/container/container';
-import PageHeader from '../components/pageHeader/pageHeader';
+import Hero from '../components/hero/hero';
 import SEO from '../components/seo';
 
 const NotFoundPage = () => {
 	return (
 		<Layout>
 			<SEO title="404 not found" description="The page you're looking for was moved or doesn't exist." />
+			<Hero title="404 not found">The page you're looking for was moved or doesn't exist.</Hero>
 			<Container>
-				<PageHeader title="404 not found">The page you're looking for was moved or doesn't exist.</PageHeader>
 				<Link to="/" className="h2">
 					Go back to the frontpage
 				</Link>
