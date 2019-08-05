74c6c05e6af8ee3a7d7884dd0594d6789395b49f
Sebastian <sebastian@hamse.fi>
2019-08-05 10:12:10 +0300

    Add header shadow with state

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Logo from '../logo/logo';
 import Menu from '../menu/menu';
@@ -6,13 +6,23 @@ import Container from '../container/container';
 
 import './header.scss';
 
-const Header = () => (
-	<header className="header" role="banner">
-		<Container containerClass="header__content">
-			<Logo />
-			<Menu />
-		</Container>
-	</header>
-);
+const Header = () => {
+	const [ isPageTop, setPageTop ] = useState(true);
+
+	useEffect(() => {
+		window.addEventListener('scroll', () => {
+			setPageTop(window.pageYOffset !== 0 ? false : true);
+		});
+	});
+
+	return (
+		<header className={`header ${!isPageTop ? 'header--shadow' : ''}`} role="banner">
+			<Container containerClass="header__content">
+				<Logo />
+				<Menu />
+			</Container>
+		</header>
+	);
+};
 
 export default Header;
