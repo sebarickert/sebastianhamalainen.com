914ca7326fa720f9325c6dbef86c8ed04e9eb302
Sebastian <sebastian@hamse.fi>
2019-09-16 20:26:24 +0300

    Add og:image for blog posts

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -9,53 +9,53 @@ import LinkContainerLink from '../components/linkContainer/linkContainer.link';
 import './blog.scss';
 
 export const query = graphql`
-	query($slug: String!) {
-		markdownRemark(fields: { slug: { eq: $slug } }) {
-			frontmatter {
-				title
-				date(formatString: "MMMM DD, YYYY")
-				image {
-					childImageSharp {
-						fluid(maxWidth: 1600, maxHeight: 600, quality: 100) {
-							src
-						}
-					}
-				}
-			}
-			html
-			timeToRead
-			excerpt
-		}
-	}
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        image {
+          childImageSharp {
+            fluid(maxWidth: 1600, maxHeight: 600, quality: 100) {
+              src
+            }
+          }
+        }
+      }
+      html
+      timeToRead
+      excerpt
+    }
+  }
 `;
 
 const Blog = ({ data }) => {
-	const { markdownRemark } = data;
-	const { frontmatter, excerpt, timeToRead, html } = markdownRemark;
-	const { title, date, image } = frontmatter;
-	const imageSource = image ? image.childImageSharp.fluid.src : '';
-	return (
-		<Layout>
-			<SEO title={`${title} | Blog`} description={excerpt} />
-			<Container>
-				<div className="blog__image">
-					<img src={imageSource} alt="" role="presentation" />
-				</div>
-			</Container>
-			<Container containerClass="container--small">
-				<LinkContainer>
-					<LinkContainerLink linkTarget="/blog" linkContainerLinkClass="mb--2">
-						Go back to Blog
-					</LinkContainerLink>
-				</LinkContainer>
-				<h1 className="blog__heading">{title}</h1>
-				<span className="blog__published">
-					{date} – {timeToRead} min read
-				</span>
-				<div className="blog__content" dangerouslySetInnerHTML={{ __html: html }} />
-			</Container>
-		</Layout>
-	);
+  const { markdownRemark } = data;
+  const { frontmatter, excerpt, timeToRead, html } = markdownRemark;
+  const { title, date, image } = frontmatter;
+  const imageSource = image ? image.childImageSharp.fluid.src : '';
+  return (
+    <Layout>
+      <SEO title={`${title} | Blog`} description={excerpt} image={imageSource} />
+      <Container>
+        <div className="blog__image">
+          <img src={imageSource} alt="" role="presentation" />
+        </div>
+      </Container>
+      <Container containerClass="container--small">
+        <LinkContainer>
+          <LinkContainerLink linkTarget="/blog" linkContainerLinkClass="mb--2">
+            Go back to Blog
+          </LinkContainerLink>
+        </LinkContainer>
+        <h1 className="blog__heading">{title}</h1>
+        <span className="blog__published">
+          {date} – {timeToRead} min read
+        </span>
+        <div className="blog__content" dangerouslySetInnerHTML={{ __html: html }} />
+      </Container>
+    </Layout>
+  );
 };
 
 export default Blog;
