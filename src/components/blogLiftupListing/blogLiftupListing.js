86bf79805fd1343d05636f67d65661732db8d09d
Sebastian <sebastian@hamse.fi>
2019-09-16 20:06:41 +0300

    Update listing link text

--- src/components/blogLiftupListing/blogLiftupListing.js
+++ src/components/blogLiftupListing/blogLiftupListing.js
@@ -6,50 +6,50 @@ import Listing from '../listing/listing';
 import Button from '../button/button';
 
 const BlogLiftupListing = () => {
-	const data = useStaticQuery(graphql`
-		{
-			allMarkdownRemark(
-				sort: { fields: [frontmatter___date], order: DESC }
-				filter: { frontmatter: { type: { eq: "blog" } } }
-				limit: 2
-			) {
-				edges {
-					node {
-						frontmatter {
-							title
-							date(formatString: "MMMM DD, YYYY")
-							type
-							image {
-								childImageSharp {
-									fluid(maxWidth: 1000, maxHeight: 450, quality: 100) {
-										src
-									}
-								}
-							}
-						}
-						id
-						excerpt
-						fields {
-							slug
-						}
-					}
-				}
-			}
-		}
-	`);
+  const data = useStaticQuery(graphql`
+    {
+      allMarkdownRemark(
+        sort: { fields: [frontmatter___date], order: DESC }
+        filter: { frontmatter: { type: { eq: "blog" } } }
+        limit: 2
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              date(formatString: "MMMM DD, YYYY")
+              type
+              image {
+                childImageSharp {
+                  fluid(maxWidth: 1000, maxHeight: 450, quality: 100) {
+                    src
+                  }
+                }
+              }
+            }
+            id
+            excerpt
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
 
-	return (
-		<React.Fragment>
-			<Listing
-				arrayOfContent={data.allMarkdownRemark.edges}
-				listingComponent={BlogTeaser}
-				listingClass="listing--col-2"
-			/>
-			<Button linkTo="/blog" buttonClass="button--cta">
-				See all posts
-			</Button>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <Listing
+        arrayOfContent={data.allMarkdownRemark.edges}
+        listingComponent={BlogTeaser}
+        listingClass="listing--col-2"
+      />
+      <Button linkTo="/blog" buttonClass="button--cta">
+        See all blog posts
+      </Button>
+    </React.Fragment>
+  );
 };
 
 export default BlogLiftupListing;
