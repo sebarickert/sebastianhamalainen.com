8543bdb83fa580d5d4b7ef1b31977b94c8dd7855
Sebastian <sebastian@hamse.fi>
2019-11-14 22:08:52 +0200

    Update footer structure and look

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,53 +5,52 @@ import './footer.scss';
 import './menu-footer.scss';
 
 const Footer = () => {
-	const data = useStaticQuery(graphql`
-		{
-			site {
-				siteMetadata {
-					email
-					twitter
-					linkedin
-					github
-				}
-			}
-		}
-	`);
-	return (
-		<footer id="contact" className="footer">
-			<div className="footer__content container container--inset">
-				<div className="footer__item">
-					<h2 className="footer__heading">Get in touch</h2>
-					<a href={`mailto:${data.site.siteMetadata.email}`} className="footer__link">
-						{data.site.siteMetadata.email}
-					</a>
-				</div>
-				<div className="footer__item">
-					<h2 className="footer__heading">On the web</h2>
-					<nav aria-label="Footer Navigation">
-						<ul className="menu-footer">
-							<li className="menu-footer__item">
-								<a href={data.site.siteMetadata.twitter} className="footer__link">
-									Twitter
-								</a>
-							</li>
-							<li className="menu-footer__item">
-								<a href={data.site.siteMetadata.github} className="footer__link">
-									Github
-								</a>
-							</li>
-							<li className="menu-footer__item">
-								<a href={data.site.siteMetadata.linkedin} className="footer__link">
-									LinkedIn
-								</a>
-							</li>
-						</ul>
-					</nav>
-				</div>
-				<span className="footer__copyright">© 2019 Sebastian</span>
-			</div>
-		</footer>
-	);
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          email
+          twitter
+          linkedin
+          github
+        }
+      }
+    }
+  `);
+  return (
+    <footer id="contact" className="footer">
+      <div className="footer__content">
+        <div className="footer__item footer__item--top container container--inset">
+          <h4 className="footer__heading">
+            <a href={`mailto:${data.site.siteMetadata.email}`} className="footer__link footer__link--large">
+              {data.site.siteMetadata.email}
+            </a>
+          </h4>
+        </div>
+        <div className="divider" />
+        <div className="footer__item footer__item--bottom container container--inset">
+          <ul className="menu-footer">
+            <li className="menu-footer__item">
+              <a href={data.site.siteMetadata.twitter} className="footer__link">
+                Twitter
+              </a>
+            </li>
+            <li className="menu-footer__item">
+              <a href={data.site.siteMetadata.github} className="footer__link">
+                Github
+              </a>
+            </li>
+            <li className="menu-footer__item">
+              <a href={data.site.siteMetadata.linkedin} className="footer__link">
+                LinkedIn
+              </a>
+            </li>
+          </ul>
+          <span className="footer__copyright">© 2019 Sebastian. All Rights Reserved.</span>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
