107d46e2c60901d6363c9ceec5655bb42f070ed2
Sebastian <sebastian@hamse.fi>
2019-11-15 12:30:06 +0200

    Revert button.js

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -3,22 +3,10 @@ import { Link } from 'gatsby';
 
 import './button.scss';
 
-var isExternal = (url) => {
-  return !(
-    window.location.href.replace('http://', '').replace('https://', '').split('/')[0] ===
-    url.replace('http://', '').replace('https://', '').split('/')[0]
-  );
-};
-
-const Button = ({ linkTo, buttonClass, children }) =>
-  !isExternal(linkTo) ? (
-    <Link to={linkTo} className={`button ${buttonClass ? buttonClass : ''}`}>
-      {children}
-    </Link>
-  ) : (
-    <a href={linkTo} className={`button ${buttonClass ? buttonClass : ''}`}>
-      {children}
-    </a>
-  );
+const Button = ({ linkTo, buttonClass, children }) => (
+  <Link to={linkTo} className={`button ${buttonClass ? buttonClass : ''}`}>
+    {children}
+  </Link>
+);
 
 export default Button;
