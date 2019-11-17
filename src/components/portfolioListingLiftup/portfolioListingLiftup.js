ae15781a7ea1f49be0f7a7ba00d6629530ab7b49
Sebastian <sebastian@hamse.fi>
2019-11-17 18:03:51 +0200

    Fix IE browser issue

--- src/components/portfolioListingLiftup/portfolioListingLiftup.js
+++ src/components/portfolioListingLiftup/portfolioListingLiftup.js
@@ -6,49 +6,49 @@ import Listing from '../listing/listing';
 import Button from '../button/button';
 
 const PortfolioListingLiftup = () => {
-	const data = useStaticQuery(graphql`
-		{
-			allMarkdownRemark(
-				sort: { fields: [frontmatter___date], order: DESC }
-				filter: { frontmatter: { type: { eq: "portfolio" } } }
-				limit: 3
-			) {
-				edges {
-					node {
-						frontmatter {
-							title
-							lead
-							date(formatString: "MMMM DD, YYYY")
-							teaser_image {
-								childImageSharp {
-									fluid(quality: 100) {
-										src
-									}
-								}
-							}
-						}
-						id
-						fields {
-							slug
-						}
-					}
-				}
-			}
-		}
-	`);
+  const data = useStaticQuery(graphql`
+    {
+      allMarkdownRemark(
+        sort: { fields: [frontmatter___date], order: DESC }
+        filter: { frontmatter: { type: { eq: "portfolio" } } }
+        limit: 3
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              lead
+              date(formatString: "MMMM DD, YYYY")
+              teaser_image {
+                childImageSharp {
+                  fluid(quality: 100) {
+                    src
+                  }
+                }
+              }
+            }
+            id
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
 
-	return (
-		<React.Fragment>
-			<Listing
-				arrayOfContent={data.allMarkdownRemark.edges}
-				listingComponent={PortfolioTeaser}
-				listingClass="listing--col-3"
-			/>
-			<Button linkTo="/portfolio" buttonClass="button--cta">
-				See all portfolio showcases
-			</Button>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <Listing
+        arrayOfContent={data.allMarkdownRemark.edges}
+        listingComponent={PortfolioTeaser}
+        listingClass="listing--col-3"
+      />
+      <Button linkTo="/portfolio" buttonClass="button--cta">
+        See all portfolio showcases
+      </Button>
+    </React.Fragment>
+  );
 };
 
 export default PortfolioListingLiftup;
