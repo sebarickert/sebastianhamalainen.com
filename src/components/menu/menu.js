33e37c1118d102556ac6314f527c8cdf5301667c
Sebastian <sebastian@hamse.fi>
2019-11-17 18:08:47 +0200

    Update main menu link order

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -5,66 +5,56 @@ import './menu-main.scss';
 import './hamburger.scss';
 
 const Menu = () => {
-	const [ isOpen, setIsOpen ] = useState(false);
-	const toggle = () => setIsOpen(!isOpen);
+  const [ isOpen, setIsOpen ] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
-	useEffect(() => {
-		// Lock page scrolling when mobile menu is open.
-		const bodyElement = document.querySelector('body');
-		bodyElement.style.overflow = isOpen ? 'hidden' : 'initial';
-	});
+  useEffect(() => {
+    // Lock page scrolling when mobile menu is open.
+    const bodyElement = document.querySelector('body');
+    bodyElement.style.overflow = isOpen ? 'hidden' : 'initial';
+  });
 
-	return (
-		<React.Fragment>
-			<div className={`overlay-toggle-container js-overlay-toggle-container ${isOpen ? 'is-active' : ''}`}>
-				<span className="overlay-toggle-label js-overlay-toggle-label">{isOpen ? 'Close' : 'Menu'}</span>
-				<button
-					className={`overlay-toggle js-overlay-toggle ${isOpen ? 'is-active' : ''}`}
-					aria-label="menu"
-					type="button"
-					onClick={toggle}
-				>
-					<span tabIndex="-1">
-						<div className="hamburger" />
-					</span>
-				</button>
-			</div>
-			<nav className={`overlay-menu js-overlay-menu ${isOpen ? 'is-open' : ''}`} aria-label="Main Navigation">
-				<ul className="menu-main">
-					<li className="menu-main__item">
-						<Link to="/" className="menu-main__link" activeClassName="is-active">
-							<span>Home</span>
-						</Link>
-					</li>
-					<li className="menu-main__item">
-						<Link
-							to="/about"
-							className="menu-main__link"
-							activeClassName="is-active"
-							partiallyActive={true}
-						>
-							<span>About</span>
-						</Link>
-					</li>
-					<li className="menu-main__item">
-						<Link to="/blog" className="menu-main__link" activeClassName="is-active" partiallyActive={true}>
-							<span>Blog</span>
-						</Link>
-					</li>
-					<li className="menu-main__item">
-						<Link
-							to="/portfolio"
-							className="menu-main__link"
-							activeClassName="is-active"
-							partiallyActive={true}
-						>
-							<span>Portfolio</span>
-						</Link>
-					</li>
-				</ul>
-			</nav>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <div className={`overlay-toggle-container js-overlay-toggle-container ${isOpen ? 'is-active' : ''}`}>
+        <span className="overlay-toggle-label js-overlay-toggle-label">{isOpen ? 'Close' : 'Menu'}</span>
+        <button
+          className={`overlay-toggle js-overlay-toggle ${isOpen ? 'is-active' : ''}`}
+          aria-label="menu"
+          type="button"
+          onClick={toggle}
+        >
+          <span tabIndex="-1">
+            <div className="hamburger" />
+          </span>
+        </button>
+      </div>
+      <nav className={`overlay-menu js-overlay-menu ${isOpen ? 'is-open' : ''}`} aria-label="Main Navigation">
+        <ul className="menu-main">
+          <li className="menu-main__item">
+            <Link to="/" className="menu-main__link" activeClassName="is-active">
+              <span>Home</span>
+            </Link>
+          </li>
+          <li className="menu-main__item">
+            <Link to="/about" className="menu-main__link" activeClassName="is-active" partiallyActive={true}>
+              <span>About</span>
+            </Link>
+          </li>
+          <li className="menu-main__item">
+            <Link to="/portfolio" className="menu-main__link" activeClassName="is-active" partiallyActive={true}>
+              <span>Portfolio</span>
+            </Link>
+          </li>
+          <li className="menu-main__item">
+            <Link to="/blog" className="menu-main__link" activeClassName="is-active" partiallyActive={true}>
+              <span>Blog</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </React.Fragment>
+  );
 };
 
 export default Menu;
