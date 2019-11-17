445a7168958af75d783b73821aa2a2bf793a060a
Sebastian <sebastian@hamse.fi>
2019-11-17 21:56:46 +0200

    Update blog teaser look

--- src/components/blogTeaser/blogTeaser.js
+++ src/components/blogTeaser/blogTeaser.js
@@ -5,27 +5,29 @@ import './blog-teaser.scss';
 import ArrowRightIcon from '../../assets/right-arrow.svg';
 
 const BlogTeaser = ({ data }) => {
-	const { node } = data;
-	const { frontmatter, fields, excerpt } = node;
-	const { title, date, image } = frontmatter;
-	const imageSource = image ? image.childImageSharp.fluid.src : '';
-	return (
-		<div className="blog-teaser">
-			<Link to={`/blog/${fields.slug}`} className="blog-teaser__link">
-				<div className="blog-teaser__container">
-					<img className="blog-teaser__image" src={imageSource} alt="" role="presentation" />
-					<div className="blog-teaser__overlay">
-						<ArrowRightIcon className="blog-teaser__icon" />
-					</div>
-				</div>
-				<h2 className="blog-teaser__heading">
-					<span>{title}</span>
-				</h2>
-				<span className="blog-teaser__published">{date}</span>
-				<p className="blog-teaser__lead">{excerpt}</p>
-			</Link>
-		</div>
-	);
+  const { node } = data;
+  const { frontmatter, fields, excerpt } = node;
+  const { title, date, image } = frontmatter;
+  const imageSource = image ? image.childImageSharp.fluid.src : '';
+  return (
+    <div className="blog-teaser">
+      <Link to={`/blog/${fields.slug}`} className="blog-teaser__link">
+        <div className="blog-teaser__container">
+          <img className="blog-teaser__image" src={imageSource} alt="" role="presentation" />
+          <div className="blog-teaser__overlay">
+            <ArrowRightIcon className="blog-teaser__icon" />
+          </div>
+        </div>
+        <div className="blog-teaser__content">
+          <h2 className="blog-teaser__heading">
+            <span>{title}</span>
+          </h2>
+          <span className="blog-teaser__published">{date}</span>
+          <p className="blog-teaser__lead">{excerpt}</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default BlogTeaser;
