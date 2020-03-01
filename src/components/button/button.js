d86e76753f57430ab1377f28725091b09a58d659
Sebastian <sebastian@hamse.fi>
2020-03-01 12:13:37 +0200

    Update button component

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -3,10 +3,22 @@ import { Link } from 'gatsby';
 
 import './button.scss';
 
-const Button = ({ linkTo, buttonClass, children }) => (
-  <Link to={linkTo} className={`button ${buttonClass || ''}`}>
-    {children}
-  </Link>
+const Button = ({
+  linkTo, buttonClass, linkTargetType, children,
+}) => (
+  linkTargetType !== 'external' ? (
+    <Link to={linkTo} className={`button ${buttonClass || ''}`}>
+      {children}
+    </Link>
+  ) : (
+    <a
+      href={linkTo}
+      className={`button ${buttonClass || ''}`}
+      title={children}
+    >
+      {children}
+    </a>
+  )
 );
 
 export default Button;
