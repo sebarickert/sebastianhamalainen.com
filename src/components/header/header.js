39bb1f4f3dbb0c3980d71c5de6c0b55dfca1ce90
Sebastian <sebastian@hamse.fi>
2020-03-01 11:09:11 +0200

    Remove useState from header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import Logo from '../logo/logo';
 import Menu from '../menu/menu';
@@ -6,23 +6,13 @@ import Container from '../container/container';
 
 import './header.scss';
 
-const Header = () => {
-  const [isPageTop, setPageTop] = useState(true);
-
-  useEffect(() => {
-    window.addEventListener('scroll', () => {
-      setPageTop(window.pageYOffset === 0);
-    });
-  });
-
-  return (
-    <header className={`header ${!isPageTop ? 'header--shadow' : ''}`} role="banner">
-      <Container containerClass="header__content">
-        <Logo />
-        <Menu />
-      </Container>
-    </header>
-  );
-};
+const Header = () => (
+  <header className="header" role="banner">
+    <Container containerClass="header__content">
+      <Logo />
+      <Menu />
+    </Container>
+  </header>
+);
 
 export default Header;
