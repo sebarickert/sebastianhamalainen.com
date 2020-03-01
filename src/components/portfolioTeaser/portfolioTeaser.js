be0e654661fd6485881ca7469088275496d8fe93
Sebastian <sebastian@hamse.fi>
2020-03-01 18:53:56 +0200

    Update portfolio-teaser component

--- src/components/portfolioTeaser/portfolioTeaser.js
+++ src/components/portfolioTeaser/portfolioTeaser.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import Button from '../button/button';
 import './portfolio-teaser.scss';
-import ArrowRightIcon from '../../assets/right-arrow.svg';
 
 const PortfolioTeaser = ({ frontmatter, fields }) => {
   const {
@@ -10,29 +10,29 @@ const PortfolioTeaser = ({ frontmatter, fields }) => {
   } = frontmatter;
   const { src: imageSrc } = teaserImage.childImageSharp.fluid;
   return (
-    <div className="portfolio-teaser">
-      <Link to={fields.slug} className="portfolio-teaser__link">
-        <div className="portfolio-teaser__container">
-          <img className="portfolio-teaser__image" src={imageSrc} alt="" role="presentation" />
-          <div className="portfolio-teaser__overlay">
-            <ArrowRightIcon className="portfolio-teaser__icon" />
-          </div>
-        </div>
-        <div className="portfolio-teaser__content">
-          <h2 className="portfolio-teaser__heading">
+    <article className="portfolio-teaser">
+      <div className="portfolio-teaser__container">
+        <img className="portfolio-teaser__image" src={imageSrc} alt="" role="presentation" />
+      </div>
+      <div className="portfolio-teaser__content">
+        <h2 className="portfolio-teaser__heading">
+          <Link to={fields.slug} className="portfolio-teaser__link">
             <span>{title}</span>
-          </h2>
-          {date ? (
-            <span className="portfolio-teaser__date" role="presentation">
-              {date}
-            </span>
-          ) : (
-            ''
-          )}
-          <p className="portfolio-teaser__lead">{lead}</p>
-        </div>
-      </Link>
-    </div>
+          </Link>
+        </h2>
+        {date ? (
+          <span className="portfolio-teaser__date" role="presentation">
+            {date}
+          </span>
+        ) : (
+          ''
+        )}
+        <p className="portfolio-teaser__lead">{lead}</p>
+      </div>
+      <Button linkTo={fields.slug} buttonClass="mt--2">
+        Read more
+      </Button>
+    </article>
   );
 };
 
