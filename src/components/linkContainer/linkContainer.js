46862f8f811379d6d90e810993ecdfd82f4bdf7b
Sebastian <sebastian@hamse.fi>
2020-03-01 17:04:24 +0200

    Remove LinkContainer component

--- src/components/linkContainer/linkContainer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import './link-container.scss';
-
-const LinkContainer = ({ linkContainerClass, children }) => <div className={`link-container ${linkContainerClass || ''}`}>{children}</div>;
-
-export default LinkContainer;
