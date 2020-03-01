ac6ba6bb0e3266cdf8170ee153d618b99f4f14b4
Sebastian <sebastian@hamse.fi>
2020-03-01 17:27:45 +0200

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout/layout';
 import Container from '../components/container/container';
-import Hero from '../components/hero/hero';
+import Button from '../components/button/button';
 import SEO from '../components/seo';
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404 not found" description="The page you're looking for was moved or doesn't exist." />
-    {/* eslint-disable-next-line react/no-unescaped-entities */}
-    <Hero title="404 not found">The page you're looking for was moved or doesn't exist.</Hero>
-    <Container>
-      <Link to="/" className="h3">
-        Go back to the frontpage
-      </Link>
+    <Container containerClass="container--small">
+      <h1 className="mt--4">404 not found</h1>
+      {/* eslint-disable-next-line react/no-unescaped-entities */}
+      <h2 className="h3">The page you're looking for was moved or doesn't exist.</h2>
+      <Button linkTo="/" buttonClass="mt--4">Go back to the frontpage</Button>
     </Container>
   </Layout>
 );
