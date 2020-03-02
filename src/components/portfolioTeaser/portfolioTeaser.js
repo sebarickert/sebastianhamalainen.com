6dce3401f6a5221fe206bc14611c8a75379ac5df
Sebastian <sebastian@hamse.fi>
2020-03-02 21:52:25 +0200

    Update blog and portfolio teaser component structure

--- src/components/portfolioTeaser/portfolioTeaser.js
+++ src/components/portfolioTeaser/portfolioTeaser.js
@@ -4,10 +4,9 @@ import { Link } from 'gatsby';
 import Button from '../button/button';
 import './portfolio-teaser.scss';
 
-const PortfolioTeaser = ({ frontmatter, fields }) => {
-  const {
-    title, teaser_image: teaserImage, lead, date,
-  } = frontmatter;
+const PortfolioTeaser = ({
+  title, date, teaser_image: teaserImage, lead, slug,
+}) => {
   const { src: imageSrc } = teaserImage.childImageSharp.fluid;
   return (
     <article className="portfolio-teaser">
@@ -16,7 +15,7 @@ const PortfolioTeaser = ({ frontmatter, fields }) => {
       </div>
       <div className="portfolio-teaser__content">
         <h2 className="portfolio-teaser__heading">
-          <Link to={fields.slug} className="portfolio-teaser__link">
+          <Link to={slug} className="portfolio-teaser__link">
             <span>{title}</span>
           </Link>
         </h2>
@@ -29,7 +28,7 @@ const PortfolioTeaser = ({ frontmatter, fields }) => {
         )}
         <p className="portfolio-teaser__lead">{lead}</p>
       </div>
-      <Button linkTo={fields.slug} buttonClass="mt--2">
+      <Button linkTo={slug} buttonClass="mt--2">
         Read more
       </Button>
     </article>
