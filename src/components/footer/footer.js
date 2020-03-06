0672242ff40fdf843090291a5cb852079a42a49c
Sebastian <sebastian@hamse.fi>
2020-03-06 22:41:40 +0200

    Update footer styles

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 
-import Divider from '../divider/divider';
-
 import './footer.scss';
 import './menu-footer.scss';
 
@@ -21,35 +19,32 @@ const Footer = () => {
   `);
   return (
     <footer id="contact" className="footer">
-      <div className="footer__content">
-        <div className="footer__item footer__item--top container container--inset">
-          <h4 className="footer__heading">
-            <a href={`mailto:${data.site.siteMetadata.email}`} className="footer__link footer__link--large">
-              {data.site.siteMetadata.email}
+      <div className="footer__header container container--inset">
+        <h2 className="footer__heading">
+          <a href={`mailto:${data.site.siteMetadata.email}`} className="footer__link">
+            {data.site.siteMetadata.email}
+          </a>
+        </h2>
+      </div>
+      <div className="footer__content container container--inset">
+        <ul className="menu-footer">
+          <li className="menu-footer__item">
+            <a href={data.site.siteMetadata.twitter} className="menu-footer__link">
+              <span>Twitter</span>
             </a>
-          </h4>
-        </div>
-        <Divider />
-        <div className="footer__item footer__item--bottom container container--inset">
-          <ul className="menu-footer">
-            <li className="menu-footer__item">
-              <a href={data.site.siteMetadata.twitter} className="menu-footer__link">
-                <span>Twitter</span>
-              </a>
-            </li>
-            <li className="menu-footer__item">
-              <a href={data.site.siteMetadata.github} className="menu-footer__link">
-                <span>Github</span>
-              </a>
-            </li>
-            <li className="menu-footer__item">
-              <a href={data.site.siteMetadata.linkedin} className="menu-footer__link">
-                <span>LinkedIn</span>
-              </a>
-            </li>
-          </ul>
-          <span className="footer__copyright">© 2019 Sebastian. All Rights Reserved.</span>
-        </div>
+          </li>
+          <li className="menu-footer__item">
+            <a href={data.site.siteMetadata.github} className="menu-footer__link">
+              <span>Github</span>
+            </a>
+          </li>
+          <li className="menu-footer__item">
+            <a href={data.site.siteMetadata.linkedin} className="menu-footer__link">
+              <span>LinkedIn</span>
+            </a>
+          </li>
+        </ul>
+        <span className="footer__copyright">© 2019 Sebastian. All Rights Reserved.</span>
       </div>
     </footer>
   );
