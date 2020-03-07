0a116159171a33499e734a5a017ae4fda732a487
Sebastian <sebastian@hamse.fi>
2020-03-07 13:09:51 +0200

    Add pagination for portfolio listing page

---
+++ src/components/pager/pager.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Button from '../button/button';
+
+import './pager.scss';
+
+const Pager = ({
+  numPages, currentPage, contentType,
+}) => {
+  const isFirst = currentPage === 1;
+  const isLast = currentPage === numPages;
+  const prevPage = currentPage - 1 === 1 ? `/${contentType}` : `/${contentType}/${(currentPage - 1).toString()}`;
+  const nextPage = `/${contentType}/${(currentPage + 1).toString()}`;
+  return (
+    <div className="pager">
+      {!isFirst && (
+        <Button linkTo={prevPage} className="pager__item pager__item--first">Previous</Button>
+      )}
+      {!isLast && (
+        <Button linkTo={nextPage} className="pager__item pager__item--last">Next</Button>
+      )}
+    </div>
+  );
+};
+
+export default Pager;
