628e1bddc6d01e19196de2fe558ebc2fa8383ec0
Sebastian <sebastian@hamse.fi>
2020-03-26 21:01:14 +0200

    Update blogTeaser styles

--- src/components/blogLiftupListing/blogLiftupListing.js
+++ src/components/blogLiftupListing/blogLiftupListing.js
@@ -3,6 +3,10 @@ import { graphql, useStaticQuery } from 'gatsby';
 
 import BlogTeaser from '../blogTeaser/blogTeaser';
 import Listing from '../listing/listing';
+import Spacer from '../spacer/spacer';
+import Container from '../container/container';
+import Heading from '../heading/heading';
+import Button from '../button/button';
 
 const BlogLiftupListing = () => {
   const data = useStaticQuery(graphql`
@@ -30,11 +34,18 @@ const BlogLiftupListing = () => {
   `);
 
   return (
-    <Listing
-      arrayOfContent={data.allMdx.edges}
-      listingComponent={BlogTeaser}
-      listingClass="listing--col-2"
-    />
+    <div className="blog-liftup">
+      <Container variation="small">
+        <Spacer>
+          <Heading>Latest blog posts</Heading>
+          <Listing
+            arrayOfContent={data.allMdx.edges}
+            listingComponent={BlogTeaser}
+          />
+          <Button primary center linkTo="/blog" className="mt--4">{'See blog posts -->'}</Button>
+        </Spacer>
+      </Container>
+    </div>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,24 +4,16 @@ import Layout from '../components/layout/layout';
 import SEO from '../components/seo';
 import BlogLiftupListing from '../components/blogLiftupListing/blogLiftupListing';
 import PortfolioLiftupListing from '../components/portfolioListingLiftup/portfolioListingLiftup';
-import FrontpageBlock from '../components/frontpageBlock/frontpageBlock';
 import Introduction from '../components/introduction/introduction';
-import '../scss/base.scss';
 
-// const portfolioMoreButton = { text: 'See portfolio showcases -->', url: '/portfolio' };
-// const blogMoreButton = { text: 'See blog posts -->', url: '/blog' };
+import '../scss/base.scss';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Front-end Developer, Web Design and JS enthusiast" />
     <Introduction />
     <PortfolioLiftupListing />
-    {/* <FrontpageBlock blockComponent={PortfolioLiftupListing} blockComponentClass="listing--one-liner" blockHeadingClass="heading--no-margin" moreButton={portfolioMoreButton}>
-        Latest portfolio showcases
-      </FrontpageBlock>
-      <FrontpageBlock blockComponent={BlogLiftupListing} blockComponentClass="listing--one-liner gray-light" blockHeadingClass="heading--no-margin" moreButton={blogMoreButton}>
-        Latest blog posts
-      </FrontpageBlock> */}
+    <BlogLiftupListing />
   </Layout>
 );
 
