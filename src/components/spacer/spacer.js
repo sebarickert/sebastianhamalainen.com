56b40440d9b66153e2a6b0d7d288c22444a0c2f7
Sebastian <sebastian@hamse.fi>
2020-03-26 20:16:38 +0200

    Add spacer component

---
+++ src/components/spacer/spacer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './spacer.scss';
+
+const Spacer = ({
+  children,
+}) => <div className="spacer">{children}</div>;
+
+export default Spacer;
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Layout from '../components/layout/layout';
 import Container from '../components/container/container';
+import Spacer from '../components/spacer/spacer';
 import Button from '../components/button/button';
 import SEO from '../components/seo';
 
@@ -9,10 +10,12 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404 not found" description="The page you're looking for was moved or doesn't exist." />
     <Container variation="small">
-      <h1 className="mt--4">404 not found</h1>
-      {/* eslint-disable-next-line react/no-unescaped-entities */}
-      <h2 className="h3">The page you're looking for was moved or doesn't exist.</h2>
-      <Button primary linkTo="/" className="mt--4">{'<-- Go back to the frontpage'}</Button>
+      <Spacer>
+        <h1>404 not found</h1>
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        <h2>The page you're looking for was moved or doesn't exist.</h2>
+        <Button primary linkTo="/" className="mt--4">{'<-- Go back to the frontpage'}</Button>
+      </Spacer>
     </Container>
   </Layout>
 );
