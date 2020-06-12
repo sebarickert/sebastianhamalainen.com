841cf94683824d68a59ef767a8568e20a0f0cbfc
Sebastian <sebastian@hamse.fi>
2020-06-12 22:03:30 +0300

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,17 +6,21 @@ import Spacer from '../components/spacer/spacer';
 import Button from '../components/button/button';
 import SEO from '../components/seo';
 
+import '../scss/templates/404.scss';
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404 not found" description="The page you're looking for was moved or doesn't exist." />
-    <Container variation="small">
-      <Spacer>
-        <h1>404 not found</h1>
-        {/* eslint-disable-next-line react/no-unescaped-entities */}
-        <h2>The page you're looking for was moved or doesn't exist.</h2>
-        <Button primary linkTo="/" className="mt--4">{'<-- Go back to the frontpage'}</Button>
-      </Spacer>
-    </Container>
+    <div className="not-found-page">
+      <Container variation="small">
+        <Spacer large>
+          <h1 className="not-found-page__heading">404 not found</h1>
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          <h2 className="not-found-page__lead">The page you're looking for was moved or doesn't exist.</h2>
+          <Button primary linkTo="/" className="not-found-page__button">{'<-- Go back to the frontpage'}</Button>
+        </Spacer>
+      </Container>
+    </div>
   </Layout>
 );
 
